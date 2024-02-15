bf2c60e397d23ad2762a58db0f67e7ed377c6895
KAP220 <88947023+KAP220@users.noreply.github.com>
2024-02-15 22:00:56 +0530

    first commit

---
+++ src/Login.jsx
@@ -0,0 +1,14 @@
+import "./Login.css"
+
+import React from 'react'
+
+function Login() {
+  return (
+    <div className='login' >
+        <img src='https://martech.org/wp-content/uploads/2017/09/spotify-logo-1920x1080.jpg' alt='spotify image'/>
+        <a href="#">Login with spotify</a>
+    </div>
+  )
+}
+
+export default Login

0c331dab9bce53fe25332d6cfe3789f7d6d1c691
KAP220 <88947023+KAP220@users.noreply.github.com>
2024-02-16 22:13:43 +0530

    configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Login from "./Login"
-
+import React,{useState,useEffect} from "react"
+import { getTokenFromUrl } from "./Spotify"
 
 function App() {
 
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,12 +1,12 @@
 import "./Login.css"
-
+import { loginUrl } from "./Spotify";
 import React from 'react'
 
 function Login() {
   return (
     <div className='login' >
         <img src='https://martech.org/wp-content/uploads/2017/09/spotify-logo-1920x1080.jpg' alt='spotify image'/>
-        <a href="#">Login with spotify</a>
+        <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
     </div>
   )
 }
